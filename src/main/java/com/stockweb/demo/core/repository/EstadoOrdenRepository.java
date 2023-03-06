d85c0106bea56e085646634e89d9922e330bc789
matiaguro <105395800+matiaguro@users.noreply.github.com>
2023-03-06 17:44:35 -0300

    Se integro modelo de estados para las ordenes

---
+++ src/main/java/com/stockweb/demo/core/repository/EstadoOrdenRepository.java
@@ -0,0 +1,8 @@
+package com.stockweb.demo.core.repository;
+
+import com.stockweb.demo.core.model.EstadoOrden;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoOrdenRepository extends JpaRepository<EstadoOrden, Long> {
+
+}
---
+++ src/main/resources/db/migration/V1.09.1__add_estado_orden.sql
@@ -0,0 +1,7 @@
+insert into ESTADO_ORDEN(clave_estado,desc_estado) values ("CREADA","Orden creada");
+
+insert into ESTADO_ORDEN(clave_estado,desc_estado) values ("ADELANTO","Orden con pago adelantado");
+
+insert into ESTADO_ORDEN(clave_estado,desc_estado) values ("PAGADA","Orden pagada en totalidad");
+
+insert into ESTADO_ORDEN(clave_estado,desc_estado) values ("FINALIZADA","Orden cerrada");
